527cb02a096640e2d954836c100d916aeb7eaad1
duong97 <nvduong15>
2019-05-21 15:04:59 +0700

    - Add chat with customer dialog (facebook plugin)

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -30,6 +30,28 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v3.3'
+        });
+    };
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+    attribution=setup_tool
+    page_id="1127264994113778">
+</div><!-- END Load Facebook SDK for JavaScript -->
+
 <div class="wrap">
     <!-- top menu -->
     <div class="navbar-fixed-top navbar-default nav-container">
